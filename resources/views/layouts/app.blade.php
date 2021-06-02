a92cfd0198e593c2347f9fab5092f88f5a172b5c
mio.kirameki <mio-masakidac.co.jp>
2021-06-02 11:45:40 +0000

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
         </div>
 
         <!-- JavaScriptの指定は省略 -->
-        </div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
